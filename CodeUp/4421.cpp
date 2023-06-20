1bf06a6128d67b1b25217e3765ddd8349b35f6b5
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2023-06-20 12:18:12 +0900

    230620

--- CodeUp/4421.cpp
+++ CodeUp/4421.cpp
@@ -13,6 +13,7 @@ int dx[4]={0,0,-1,1};
 int dy[4]={-1,1,0,0};
 vector<int> v;	//solve
 int house = 1;	//solve
+int label = 1;	//solve
 
 int input() {
 
@@ -34,15 +35,15 @@ int dfs(int nowx, int nowy) {
 	int count = 1;
 	check[nowx][nowy]=1;
 	
-	int(int i=0; i<4; i++) {
+	for(int i=0; i<4; i++) {
 		nx=nowx+dx[i];
 		ny=nowy+dy[i];
 		
 		if(nx<=N && nx>=1 && ny<=N && ny>=1) {
-			if(check[nx][ny]==0 && data[nowx][nowy]==0 check[nx][ny]) {
+			if(check[nx][ny]==0 && data[nowx][nowy]==1) {
 				dfs(nx, ny);
+			}
 		}
-		
 		
 	}
 	
@@ -53,15 +54,27 @@ int dfs(int nowx, int nowy) {
 int solve() {
 	for(int i=1; i<=N; i++) {
 		for(int j=1; j<=N; j++) {
-			dfs(i,j);
+			if(check[i][j]==0 && data[i][j]==1) {
+				dfs(i,j);
+				label++;
+				v.push_back(house);
+				house=0;
+			}	
 		}
-	}	
+	}
+	return 0;
 }
 
 int main() {
 	
 	input();
 	solve();
+
+	sort(v.begin(), v.end());
+	cout << label-1 << endl;
+	for(int i=0; i<v.size(); i++) {
+		cout << v[i] << endl;
+	}
 	
 	return 0;
 
