30f02954468ad2346af6d5a21c4eb29078e4c2b8
Tim <bluet75@naver.com>
2023-06-27 10:14:23 +0000

    230627_1

--- CodeUp/4421.cpp
+++ CodeUp/4421.cpp
@@ -7,78 +7,65 @@ Solve: https://scarlettb.tistory.com/81
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <string>
 using namespace std;
+ 
+const int MAX = 25;
 int N;
-int data[25][25];
-int check[25][25]={0};
-int dx[4]={0,0,-1,1};
-int dy[4]={-1,1,0,0};
-vector<int> v;	//solve
-int house = 1;	//solve
-int label = 1;	//solve
-
-int input() {
-
-	cin >> N;
-	
-	for(int i=1; i<=N; i++) {
-		for(int j=1; j<=N; j++) {
-			cin >> data[i][j];
-		}
-	}	
-	
-	return 0;
-	
+int map[MAX][MAX] = { 0, };
+bool visited[MAX][MAX] = { 0, };
+int dy[] = { 0,0,1,-1 };
+int dx[] = { 1,-1,0,0 };
+int label = 1;
+vector<int> v;
+int house = 0;
+ 
+void DFS(int y, int x) {
+    visited[y][x] = true;
+    map[y][x] = label;
+    house++;
+ 
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        
+        if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+            continue;
+ 
+        if (map[ny][nx] == 1 && visited[ny][nx] == 0) {
+            DFS(ny, nx);
+        }
+    }
 }
-
-int dfs(int nowx, int nowy) {
-	
-	int nx, ny;
-	int count = 1;
-	check[nowx][nowy]=1;
-	
-	for(int i=0; i<4; i++) {
-		nx=nowx+dx[i];
-		ny=nowy+dy[i];
-		
-		if(nx<=N && nx>=1 && ny<=N && ny>=1) {
-			if(check[nx][ny]==0 && data[nowx][nowy]==1) {
-				dfs(nx, ny);
-			}
-		}
-		
-	}
-	
-	
-	return 0;
-}
-
-int solve() {
-	for(int i=1; i<=N; i++) {
-		for(int j=1; j<=N; j++) {
-			if(check[i][j]==0 && data[i][j]==1) {
-				dfs(i,j);
-				label++;
-				v.push_back(house);
-				house=0;
-			}	
-		}
-	}
-	return 0;
-}
-
+ 
 int main() {
-	
-	input();
-	solve();
-
-	sort(v.begin(), v.end());
-	cout << label-1 << endl;
-	for(int i=0; i<v.size(); i++) {
-		cout << v[i] << endl;
-	}
-	
-	return 0;
-
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        string input;
+        cin >> input;
+ 
+        for (int j = 0; j < N; j++) {
+            map[i][j] = input.at(j) - '0';
+        }
+    }
+ 
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (map[i][j] == 1 && visited[i][j] == 0) {
+                DFS(i, j);
+                label++;
+                v.push_back(house);
+                house = 0;
+            }
+        }
+    }
+ 
+    sort(v.begin(), v.end());
+ 
+    cout << label-1 << endl;
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+ 
 }
