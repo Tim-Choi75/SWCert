2067d6fad14dea583715efc3abefe7d8ac339120
choiyunseok <choiyunseok@gmail.com>
2024-03-28 17:58:19 +0900

    280328

--- MinCoding/Day9_Bloom.cpp
+++ MinCoding/Day9_Bloom.cpp
@@ -18,8 +18,8 @@ using namespace std;
 int height, width, stx, sty, N=5;
 bool visited[MAX_N][MAX_N]={false};
 int Graph[MAX_N][MAX_N];
-int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
+int dx[4]={0,0,-1,1};
+int dy[4]={-1,1,0,0};
 queue<pair<int,int> > Q;
 
 int FloodFill() {
@@ -31,10 +31,10 @@ int FloodFill() {
 		Q.pop();
 
 		//
-		if(visited[curr.X][curr.Y] > maxi) {
-			cout <<"curr.X: "<< curr.X << " curr.Y: "<< curr.Y  <<  " visited[curr.X][curr.Y]: "<< visited[curr.X][curr.Y] <<  ">"  << maxi << endl;
-			maxi=visited[curr.X][curr.Y];
-			cout <<" After maxi: "<< maxi << endl;
+		if(Graph[curr.X][curr.Y] > maxi) {
+			//cout <<"curr.X: "<< curr.X << " curr.Y: "<< curr.Y  <<  " Graph[curr.X][curr.Y]: "<< Graph[curr.X][curr.Y] <<  ">"  << maxi << endl;
+			maxi=Graph[curr.X][curr.Y];
+			//cout <<"After maxi: "<< maxi << endl;
 		}
 			
 					
@@ -43,22 +43,22 @@ int FloodFill() {
 			int nx=curr.X+dx[i];
 			int ny=curr.Y+dy[i];
 			
-			cout <<  "i: "<< i << " curr.X: " << curr.X << " curr.Y: " << curr.Y << " nx: "<< nx << " ny: "<< ny << endl;
-			
 			//경계는 넘을수 없다 
 			if(nx<0 || nx>=height || ny<0 || ny>=width)
 				continue;
-			
+						
 			//방문한적이 있으면 skip
 			if(visited[nx][ny]==1)
 				continue;
+			
+			//cout <<  "height: "<< height << " width: " << width << " nx: "<< nx << " ny: "<< ny << endl;
 
 			visited[nx][ny]=1;
-			cout <<"curr.X: "<< curr.X << " curr.Y: "<< curr.Y  <<  " visited[curr.X][curr.Y]: "<< visited[curr.X][curr.Y] << endl;
-			cout <<"nx: "<< nx << " ny: "<< ny << " visited[nx][ny]: "<<visited[nx][ny] << endl;
-			visited[nx][ny]=visited[curr.X][curr.Y]+1;
+			//cout <<"curr.X: "<< curr.X << " curr.Y: "<< curr.Y  <<  " visited[curr.X][curr.Y]: "<< visited[curr.X][curr.Y] << endl;
+			//cout <<"nx: "<< nx << " ny: "<< ny << " visited[nx][ny]: "<<visited[nx][ny] << endl;
+			Graph[nx][ny]=Graph[curr.X][curr.Y]+1;
 			//cout <<"New Value: "<< Graph[curr.X][curr.Y]+1 << endl;
-			cout <<"nx: "<< nx << " ny: "<< ny << " After visited[nx][ny]: "<< visited[nx][ny] << endl;
+			//cout <<"nx: "<< nx << " ny: "<< ny << " After Graph[nx][ny]: "<< Graph[nx][ny] << endl;
 			
 			//새로운곳을 방문하고 거리를 늘린다. 
 			Q.push({nx, ny});
@@ -73,12 +73,11 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	int height, width;
 	cin >> height >> width;
 	
 	for (int i = 0; i <2; i++) {
 		cin >> stx >> sty;
-		cout << "stx: " << stx << " sty: " << sty << " called " << endl;
+		//cout << "stx: " << stx << " sty: " << sty << " called " << endl;
 		
 		Q.push({stx, sty});
 		visited[stx][sty]=1;
