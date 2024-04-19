c482ca12f2f461c43457fa914f8c06a4c183c187
choiyunseok <choiyunseok@gmail.com>
2024-04-19 09:41:09 +0900

    240419
    240419

---
+++ BOJ/2979.cpp
@@ -0,0 +1,19 @@
+//백준 2979 트럭 
+//https://www.acmicpc.net/problem/2979 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, B, C, s, e, arr[100];
+
+int main() {
+	
+	cin >> A >> B >> C;
+		
+	for(int i=0; i<3; i++) {
+		cin >> s >> e;
+		
+	}
+	
+	return 0;
+}
