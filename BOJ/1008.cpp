a6fb0f3677aa6fec7485c1d2b81cc4a334ac536f
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2024-04-20 00:01:37 +0900

    240420_Mac
    240420_Mac

---
+++ BOJ/1008.cpp
@@ -0,0 +1,17 @@
+//น้มุ 1008 - A/B
+//https://www.acmicpc.net/problem/1008
+
+#include <bits/stdc++.h>
+using namespace std;
+
+float A, B;
+float answer;
+
+int main() {
+
+    cin >> A >> B;
+
+     answer = A/B;
+     cout << answer;
+    return 0;
+}
