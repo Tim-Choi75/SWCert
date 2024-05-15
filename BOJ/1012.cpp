17a6acc8b516b83dfac9b0c1d1b7ee8ba0a94578
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2024-05-15 17:46:53 +0900

    240515_Mac
    240515_Mac

---
+++ BOJ/1012.cpp
@@ -0,0 +1,27 @@
+//백준 1012 - 유기농 배추
+//https://www.acmicpc.net/problem/1012
+//
+
+#include <bits/stdc++.h>
+#define 
+using namespace std;
+
+int T, M, N, K;
+
+int main() {
+
+    cin >> T;
+    for(int i=0; i<T; i++) {
+        cin >> M >> N >> K;
+        for(int i=0; i<N; i++) {
+            for(int j=0; i<M; i++) {
+
+            }
+        }
+
+    }
+    
+
+
+
+}
