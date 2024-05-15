a4f86f2ee0cd8d3c50a66642afd83a4e8b65c261
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2024-05-15 17:58:43 +0900

    240515_Mac
    240515_Mac

--- BOJ/1012.cpp
+++ BOJ/1012.cpp
@@ -3,23 +3,43 @@
 //
 
 #include <bits/stdc++.h>
-#define 
+#define MAX 51
 using namespace std;
 
 int T, M, N, K;
+int arr[MAX][MAX];
+bool check[MAX][MAX];
+
+int dx[]={}
 
 int main() {
 
     cin >> T;
+    int x, y;
     for(int i=0; i<T; i++) {
         cin >> M >> N >> K;
-        for(int i=0; i<N; i++) {
-            for(int j=0; i<M; i++) {
+        for(int i=0; i<K; i++) {
+            cin >> x >> y;
+            arr[x][y]=1;
+            arr[y][x]=1;
+        }
 
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(arr[i][j]==1) {
+                    if(!check[i][j]) {
+                        cnt++;
+                        dfs(i, j);
+                    }
+                }
             }
         }
 
-    }
+        cout << cnt << "\n";
+
+
+    }//for T
     
 
 
