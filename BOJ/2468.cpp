274d390a96bc30b87723eabf7974ab7ddd880aca
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2024-05-24 11:55:37 +0900

    240524_Mac
    240524_Mac

--- BOJ/2468.cpp
+++ BOJ/2468.cpp
@@ -12,6 +12,12 @@ using namespace std;
 int N, maxN=1; 
 int arr[MAX_N][MAX_N], visit[MAX_N]
 
+void DFS(int x, int y, int h) {
+
+}
+
+
+
 int main() {
 	
 	cin >> N;
@@ -26,11 +32,10 @@ int main() {
 	for(int h=1; h<=MAX_N-1; h++) {//1부터 최대높이-1까지 
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				cin >> arr[i][j];
-				if(arr[i][j]>=MAX_N)
-					MAX_N=arr[i][j];	
+				DFS(i, j, h);
 			}
 		}		
 	}
+	
 	return 0;
 } 
