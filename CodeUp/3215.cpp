a843c127afe1d78c405b68c25040a6696e728482
choiyunseok <choiyunseok@gmail.com>
2025-02-12 23:33:26 +0900

    250212_GPC

--- CodeUp/3215.cpp
+++ CodeUp/3215.cpp
@@ -3,8 +3,24 @@
 //Algo: 다익스트라(dijkstra)
 //풀이:
 #define _CRT_SECURE_NO_WARINGS
+#define MAX_N 27
+#define INF 987654321
 
 #include <stdio.h>
+
+int N, M, A, B, weght, start, end, i, j;
+int graph[MAX_N][MAX_N];
+int dist[MAX_N];
+
+void init_weight() {
+
+    for(i=1; i<=MAX_N; i++) {
+        for(i=1; i<=MAX_N; i++)
+    
+    }
+
+}
+
 
 int main() {
 
@@ -12,9 +28,17 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, M, begin, end, weght;
+    
     scanf("%d %d", &N, &M);
+    
+    for(i=1; i<=M; i++) {
+        scanf("%c %c %d", &A, &B, &weight);
 
+        scanf("%c %c", &start, &end);
+
+        init_weight()
+
+    }
 
 
 
