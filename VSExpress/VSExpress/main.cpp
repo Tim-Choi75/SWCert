93779e9095398833f8b5792ae6311ffbf23055d6
choiyunseok <choiyunseok@gmail.com>
2025-02-15 08:27:02 +0900

    250215_GPC
    250215_GPC

--- VSExpress/VSExpress/main.cpp
+++ VSExpress/VSExpress/main.cpp
@@ -1,32 +1,66 @@
-/*  IT취업을 위한 알고리즘_인프런
-	AlgoSolve_Inflearn
-	18. 층간 소음 */
+//CodeUp 2840 - R&# 가는길
+//https://codeup.kr/problem.php?id=2840
+//Algo: 다익스트라(dijkstra)
+//풀이:
+
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_N 10001
+#define INF 987654321
 
 #include <stdio.h>
 #include <iostream>
+#include <queue>
 using namespace std;
 
-int main() {
 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int map[MAX_N][MAX_N];
+int dist[MAX_N];
+int visit[MAX_N];
+int n, m, i, j;
 
-	int N, M, time, cnt=0;
-	freopen("D:/Git/SWCert/input/AlgoSolve_Inflearn-18.txt", "r", stdin);
-	scanf("%d %d", &N, &M);
+typedef pair<int, int> pii;
 
-	printf("N: %d M: %d", N, M);
-
-	for (int i = 1; i <= N; i++) {
-		scanf("%d", &time);
-
-		if (time > M) {
-			cnt++;
+void init_array() {
+	//배열을 초기화한다.
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			if (i != j)
+				map[i][j] = INF;
 		}
 	}
 
-	printf("cnt: %d\n", cnt);
+	for (i = 1; i <= n; i++)
+		dist[i] = INF;
+
+}
+
+void dijkstra(int src) {
+	priority_queue<pii, vector<pii>, greater<pii> > pq;
+
+	pq.push(make_pair(0, src));
+	while (!pq.empty()) {
+		int
+
+	}
+
+}
+int main() {
+
+	freopen("D:/Git/SWCert/input/CodeUp_2840.txt", "r", stdin);
+	int a, b, c;
+
+	scanf("%d %d", &n, &m);
+	printf("n: %d, m: %d", n, m);
+
+	init_array();
+	for (i = 1; i <= m; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		map[a][b] = c;
+	}
+
+	dijkstra(1);
+
+
 
 	return 0;
 }
