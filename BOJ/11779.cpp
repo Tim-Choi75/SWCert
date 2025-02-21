a3fb50eba210eb3bdfe87cf2918d597519064c71
choiyunseok <choiyunseok@gmail.com>
2025-02-21 23:27:27 +0900

    250221_GPC
    250221_GPC

--- BOJ/11779.cpp
+++ BOJ/11779.cpp
@@ -1,7 +1,31 @@
-//백준 2696 중앙값 구하기 
-//https://www.acmicpc.net/problem/2696
+//백준(BOJ) 11779 - 최소비용 구하기 2
+//https://www.acmicpc.net/problem/11779
 //Algo: Dijkstra
 //DataStructure: Priority Queue
-//Solu: https://bbeomgeun.tistory.com/136
+//Solu: 
 //Result:
 
+
+
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+#define MAX_N 27
+#define INF 987654321
+
+int n, m, i, j;
+int map[MAX_N][MAX_N];
+int dist[MAX_N];
+int visit[MAX_N];
+
+int main() {
+
+	freopen("D:/Git/SWCert/input/CodeUp_3216.txt", "r", stdin);
+
+
+	return 0;
+}
---
+++ VSExpress/VSExpress/BOJ_11779.cpp
@@ -0,0 +1,31 @@
+//백준(BOJ) 11779 - 최소비용 구하기 2
+//https://www.acmicpc.net/problem/11779
+//Algo: Dijkstra
+//DataStructure: Priority Queue
+//Solu: 
+//Result:
+
+
+
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+#define MAX_N 27
+#define INF 987654321
+
+int n, m, i, j;
+int map[MAX_N][MAX_N];
+int dist[MAX_N];
+int visit[MAX_N];
+
+int main() {
+
+	freopen("D:/Git/SWCert/input/CodeUp_3216.txt", "r", stdin);
+
+
+	return 0;
+}
