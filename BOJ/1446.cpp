45be0d96f638129164c86ad7205f8a650221497f
choiyunseok <choiyunseok@gmail.com>
2025-03-09 23:43:55 +0900

    250309_GPC

--- BOJ/1446.cpp
+++ BOJ/1446.cpp
@@ -19,7 +19,7 @@ int dist[MAX_N];
 
 void dijkstra(int st) {
 
-	priority_queue<pii, vector<pii> greater<pii>> pq;
+	priority_queue <pii, vector<pii>, greater<pii>> pq;
 	pq.push(make_pair(0, st));
 	dist[st] = 0;
 
@@ -29,7 +29,15 @@ void dijkstra(int st) {
 
 		pq.pop();
 		
-		for()
+		for (i = 1; i < v[curr].size(); i++) {
+			int next = v[curr][i].first;	//다음 정점
+			int ncost = cost + v[curr][i].second;	//다음 정점
+
+			if (dist[next] > ncost) {
+				dist[next] = ncost;
+				pq.push(make_pair(ncost, next));
+			}//if
+		}//for
 	}
 
 }
@@ -38,13 +46,20 @@ int main() {
 	
 	freopen("D:/Git/SWCert/input/BOJ_1446.txt", "r", stdin);
 	//freopen("D:/Git/SWCert/input/BOJ_1446_1.txt", "r", stdin);
-
+	
 	scanf("%d %d", &N, &D);
+	printf("N: %d, D: %d\n", N, D);
 
 	int a, b, c;
 
 	for (i = 0; i < N; i++) {
 		scanf("%d %d %d", &a, &b, &c);
+
+		if (b > D) {
+			printf("a: %d, b: %d, c: %d, D: %d\n", a, b, c, D);
+			continue;
+		}
+		
 		v[a].push_back(make_pair(b, c));
 	}
 
