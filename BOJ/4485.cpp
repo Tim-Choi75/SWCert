dc5143f325a7b986f9c9a4e1a855f3efbe4cde43
Tim Choi <bluet75@naver.com>
2025-03-15 12:38:22 +0900

    250315_GBook

---
+++ BOJ/4485.cpp
@@ -0,0 +1,21 @@
+//최단경로 
+//https://www.acmicpc.net/problem/4485
+//Algo: Dijkstra
+//Solu:https://yabmoons.tistory.com/376
+//Result:
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+
+int N;
+
+
+int main()
+{
+
+	freopen("D:/Git/SWCert/input/BOJ_4485.txt", "r", stdin);
+
+	scanf("%d", &N);
+}
