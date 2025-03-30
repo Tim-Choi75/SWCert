a923f49ad660df3db72e4ad19612eb94d1a07470
Tim Choi <bluet75@naver.com>
2025-03-30 17:12:47 +0900

    250330_GBook
    250330_GBook

--- BOJ/10282.cpp
+++ BOJ/10282.cpp
@@ -30,32 +30,32 @@ void dijkstra(int x) {
 
 	priority_queue <pii, vector<pii>, greater<pii>> pq;
 	times[x] = 0;	//자기자신을 0으로 설정
-	
+
 	pq.push(make_pair(0, x));
 
-	while(!pq.empty()) {
-		int t = pq.top().first;		//현재 시간
+	while (!pq.empty()) {
+		int t = pq.top().first;			//현재 시간
 		int cnt = pq.top().second;		//현재 노드
-	
+
 		pq.pop();
 
-		//최단 거리가 아닐경우엔 굳이 안해도 됨?
-		if (times[cnt] < t)
-			continue;
 
 		for (i = 0; i < v[cnt].size(); i++) {
-			int next = v[cnt][i].second;				//다음 노드
 			int nextTime = v[cnt][i].first + t;	//현재 시간+ 가는시간
+			int next = v[cnt][i].second;		//다음 노드
 
 			if (times[next] > nextTime)
 				times[next] = nextTime;
 				pq.push(make_pair(nextTime, next));	//큐에 담음
 		}
-	
+
 	}//while
 
 	int com = 0, t = 0;
 	for (i = 1; i <= n; i++) {
+		if (times[i] == INF)
+			continue;
+
 		com++;
 		t = max(t, times[i]);
 	}
@@ -69,12 +69,14 @@ void dijkstra(int x) {
 int main() {
 
 	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_10282.txt", "r", stdin);
-	freopen("D:/Git/SWCert/input/BOJ_11779.txt", "r", stdin);
+	freopen("D:/Git/SWCert/input/BOJ_10282.txt", "r", stdin);
 
 	scanf("%d", &tc);
+	printf("tc: %d\n", tc);
 
 	for (i = 1; i <= tc; i++) {
 		scanf("%d %d %d", &n, &d, &c);	//n:노드수, d: 의존성, c: 해킹당한 컴퓨터
+		printf("n: %d, d: %d, c: %d\n", n, d, c);	//n:노드수, d: 의존성, c: 해킹당한 컴퓨터
 
 		for (j = 1; j <= n; j++) {
 			times[j] = INF;
@@ -82,10 +84,10 @@ int main() {
 
 		for (j = 1; j <= d; j++) {
 			scanf("%d %d %d", &target, &start, &cost);
-			v[target].push_back(make_pair(cost, start));
+			v[start].push_back(make_pair(cost, target));
 		}
 
-		dijkstra(c);		
+		dijkstra(c);
 	}
 
 	return 0;
