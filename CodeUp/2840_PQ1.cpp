492335952b5a2fb96f858e8d9ddf0d7f00f7e8b6
choiyunseok <choiyunseok@gmail.com>
2025-04-03 23:48:02 +0900

    250403_GPC
    250403_GPC

--- CodeUp/2840_PQ1.cpp
+++ CodeUp/2840_PQ1.cpp
@@ -1,7 +1,7 @@
 //CodeUp 2840 - R&E 가는길
 //https://codeup.kr/problem.php?id=2840
 //Algo: 다익스트라(dijkstra)
-//풀이:
+//풀이://https://www.youtube.com/watch?v=611B-9zk2o4&t=84s
 
 
 #define  _CRT_SECURE_NO_WARNINGS
@@ -23,7 +23,7 @@ int dist[MAX_N], visit[MAX_N];
 int main() {
 
 	freopen("D:/Git/SWCert/input/CodeUp_2840.txt", "r", stdin);
-	int n, m, i, v;
+	int n, m, i;
 
 	scanf("%d %d", &n, &m);
 
@@ -38,21 +38,29 @@ int main() {
 		dist[i] = INF;
 		visit[i] = 0;
 	}
+
 	
+	pq.push(({ 0, 1 });
+	dist[1] = 0;
 	priority_queue <pii, vector<pii>, greater<pii>> pq;
-	pq.push(({0, 1});
 
 	while (!pq.empty()) {
-
-		int u = pq.top().second;	//현재 정점
-
-		if (visited[u]==1)
-			continue;
 		
-		visited[u] = 1;	//방문처리
+		int cost = pq.top().first;	//현재 시간
+		int curr = pq.top().second;	//현재 정점
 
-		for (v = 1; v <= n; v++) {
-			if(dist[v]
+		pq.pop();
+
+		//최단 거리가 아닌 경우는 skip		
+		if (dist[curr] < cost)
+			continue;
+
+		for (i = 1; i < v[curr].size(); i++) {
+			//선택된 노드의 인접 노드
+			int next = v[curr][i].first;
+			//선택된 노드 거쳐서 인접노트고 가는 비용
+			int nextCost = cost + v[curr][i].second;
+			if(nextCost)
 		}
 
 
