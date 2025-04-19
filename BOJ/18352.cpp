42b7c2d93ff778c574ab3182e4f03c5c7e5db043
choiyunseok <choiyunseok@gmail.com>
2025-04-19 18:20:15 +0900

    250419_GPC
    250419_GPC

--- BOJ/18352.cpp
+++ BOJ/18352.cpp
@@ -1,7 +1,7 @@
 //백준(BOJ) 18352 - 특정 거리의 도시
 //https://www.acmicpc.net/problem/18352
 //Algo: Dijkstra
-//Solve: 
+//Solve: https://tinyurl.com/29qc4bje
 
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -19,51 +19,56 @@ typedef pair<int, int> pii;
 
 int main() {
 
-	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_18532.txt", "r", stdin);
-	freopen("D:/Git/SWCert/input/BOJ_18532.txt", "r", stdin);
-	//freopen("D:/Git/SWCert/input/BOJ_18532_1.txt", "r", stdin);
-	//freopen("D:/Git/SWCert/input/BOJ_18532_2.txt", "r", stdin);
+	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_18352.txt", "r", stdin);
+	freopen("D:/Git/SWCert/input/BOJ_18352.txt", "r", stdin);
+	//freopen("D:/Git/SWCert/input/BOJ_18352_1.txt", "r", stdin);
+	//freopen("D:/Git/SWCert/input/BOJ_18352_2.txt", "r", stdin);
 
 	//도시의 개수 N, 도로의 개수 M, 거리 정보 K, 출발도시 X
 	int N, M, K, X, i;
 
 	scanf("%d %d %d %d", &N, &M, &K, &X);
+	printf("N: %d, M: %d, K: %d, X: %d\n", N, M, K, X);
 
-	vector<pii> v[N];
+	vector<int> v[MAX_N];
 	vector<int> dist(N + 1, INF);	//거리벡터 초기화
 
 	int from, to;
 	for (i = 1; i <= M; i++) {
 		scanf("%d %d", &from, &to);
-		v[from].push_back({ to, 1 });
+		v[from].push_back(to);
 	}
 
-	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	queue<int> q;
+	q.push(X);
 
-	dist[1] = 0;
-	pq.push({ 1, 0 });
+	dist[X] = 0;
+	pq.push(X);
 
-	while (!pq.empty()) {
+	while (!q.empty()) {
 
-		int now = pq.top().first;
-		int nowCost = pq.top().second;
-
-		pq.pop();
+		int now = q.front();
+		q.pop();
 
 		for (i = 0; i < v[now].size(); i++) {
-			int next = v[now][i].first;
-			int nextCost = nowCost + v[now][i].second;
+			int next = v[now][i]			
 
-			if (dist[next] > nextCost) {
-				dist[next] = nextCost;
-				pq.push({ next, nextCost });
+			if (dist[next] > dist[now]+1) {
+				dist[next] > dist[now] + 1;
+				q.push(next);
 			}//if
 		}//for
 	}//while
 
+	int cnt = -1;
 	for (i = 1; i <= N; i++) {
-		if (dist[i] == K)
+		if (dist[i] == K) {
 			printf("%d\n", i);
+			cnt++;
+		}
+		else {
+			printf("%d\n", cnt);
+		}			
 	}
 
 	return 0;
