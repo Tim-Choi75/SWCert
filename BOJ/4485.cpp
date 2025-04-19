4f165a3c45ff37e4ece05fa82c10a59df39ca3f2
choiyunseok <choiyunseok@gmail.com>
2025-04-19 22:51:32 +0900

    250419_GPC
    250419_GPC

--- BOJ/4485.cpp
+++ BOJ/4485.cpp
@@ -18,49 +18,48 @@ typedef pair<int, int> pii;
 vector<pii> v[MAX_N];
 int map[MAX_N][MAX_N];
 int dist[MAX_N][MAX_N];
-int dx[]={0, 0, 1, -1};
-int dy[]={1, -1, 0, -0};
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, -0 };
 
 int tc = 1, ans;
 
 void dijkstra() {
 
-
 	priority_queue<pair<int, pii>> pq;
 	pq.push(make_pair(-map[0][0], make_pair(0, 0)));
 
-	dist[0][0]=map[0][0];
+	dist[0][0] = map[0][0];
 
 	while (!pq.empty()) {
-		int cost = pq.top().first;	
+		int cost = -pq.top().first;
 		int x = pq.top().second.first;		//현재 비용
 		int y = pq.top().second.second;		//현재 정점
 		printf("cost: %d, x: %d, y: %d\n", cost, x, y);
 
 		pq.pop();
-		
+
 		//printf("v.[now].size: %d", v[now].size());
 
-		for (i = 0; i<4; i++) {
-			int nx = x+dx[i];
-			int ny = y+dy[i];
+		for (i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 
-			if(nx>=0 && ny>=0 && nx<N && ny<N) {
+			if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
 				int nCost = cost + map[nx][ny];
-				if(dist[nx][ny] > nCost) {
-					dist[nx][ny]=nCost;
+				if (dist[nx][ny] > nCost) {
+					dist[nx][ny] = nCost;
 					pq.push(make_pair(-dist[nx][ny], make_pair(nx, ny)));
 				}//if dist
 			}//if nx 
 		}//for i
 	}//while
-	ans=dist[N-1][N-1];
+	ans = dist[N - 1][N - 1];
 }
 
 int main()
 {
-	freopen("/Users/timchoi/Git/SWCert/input/BOJ_4485.txt", "r", stdin);
-	//freopen("D:/Git/SWCert/input/BOJ_4485.txt", "r", stdin);
+	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_4485.txt", "r", stdin);
+	freopen("D:/Git/SWCert/input/BOJ_4485.txt", "r", stdin);
 
 	while (true) {
 
@@ -70,7 +69,7 @@ int main()
 		if (N == 0)
 			break;
 
-		int rupee =0;
+		int rupee = 0;
 		//map배열 입력
 		for (i = 1; i <= N; i++) {
 			for (j = 1; j <= N; j++) {
