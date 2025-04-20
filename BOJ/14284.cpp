96eb3e430dc19d7e341a8c2ae72865449afe6367
Tim Choi <bluet75@naver.com>
2025-04-20 11:35:52 +0900

    250420_Gbook
    250420_Gbook

---
+++ BOJ/14284.cpp
@@ -0,0 +1,29 @@
+//백준(BOJ) 14284 - 간선 이어가기2
+//https://www.acmicpc.net/problem/10282
+//Algo: Dijkstra
+//DataStructure: Priority Queue
+
+
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+#include <queue>
+#include <vector>
+
+
+using namespace std;
+
+#define MAX_N	100001
+#define INF		1e9
+
+
+
+int main() {
+
+	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_14284.txt", "r", stdin);
+	freopen("D:/Git/SWCert/input/BOJ_14284.txt", "r", stdin);
+
+
+	return 0;
+}
