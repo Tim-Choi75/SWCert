6fb5d3d75b66bbffcf436e4740e8512e4d70fd86
choiyunseok <choiyunseok@gmail.com>
2025-04-28 23:07:06 +0900

    250428_GPC

--- BOJ/2211.cpp
+++ BOJ/2211.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 #include <queue>
 using namespace std;
-#define isPrint 0
+#define isPrint 1
 
 #define MAX_N	1001
 #define INF		1e9
@@ -25,7 +25,7 @@ int dist[MAX_N], visit[MAX_N], i;
 
 void dijkstra(int st) {
 
-	for (i = 1; i <= V; i++) {
+	for (i = 1; i <= N; i++) {
 		dist[i] = INF;
 		visit[i] = 0;
 	}
@@ -68,14 +68,14 @@ void dijkstra(int st) {
 int main() {
 
 	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_2211.txt", "r", stdin);
-	//freopen("D:/Git/SWCert/input/BOJ_2211.txt", "r", stdin);
+	freopen("D:/Git/SWCert/input/BOJ_2211.txt", "r", stdin);
 
 	scanf("%d %d", &N, &M);
-	if (isPrint) printf("V: %d, E: %d, P: %d\n", V, E, P);
+	if (isPrint) printf("N: %d, M: %d\n", N, M);
 
 	//A에서 B로 통신시간C
 	int A, B, C;
-	for (i = 0; i < E; i++) {
+	for (i = 0; i < N; i++) {
 		scanf("%d %d %d", &A, &B, &C);
 		v[A].push_back({ B, C });
 		v[B].push_back({ A, C });	//양방향 간선
@@ -83,28 +83,10 @@ int main() {
 
 	dijkstra(1);
 
-	for (i = 1; i <= V; i++) {
+	for (i = 1; i <= N; i++) {
 		if (isPrint) printf("%d ", dist[i]);
 	}
 
-	int dist_to_v = dist[V];
-	int dist_to_p = dist[P];
-
-	if (isPrint) printf("-------------------\n");
-	if (isPrint) printf("dist_to_v: %d, dist_to_p: %d\n", dist_to_v, dist_to_p);
-
-	dijkstra(P);
-	int dist_from_p_to_v = dist[V];
-
-	if (isPrint) printf("dist_from_p_to_v: %d\n", dist_from_p_to_v);
-
-	//건우(P)를 거쳐가는 경로와 아닌 경로의 최단거리를 비교
-	if (dist_to_p + dist_from_p_to_v <= dist_to_v) {
-		printf("SAVE HIM\n");
-	}
-	else {
-		printf("GOOD BYE\n");
-	}
 
 	return 0;
 }
