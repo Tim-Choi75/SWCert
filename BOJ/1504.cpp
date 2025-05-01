9f06794fb559edc9e7c4b5b7bbbde5cbad4c58d9
choiyunseok <choiyunseok@gmail.com>
2025-05-02 00:06:37 +0900

    250502_GPC
    250502_GPC

--- BOJ/1504.cpp
+++ BOJ/1504.cpp
@@ -18,38 +18,58 @@ typedef pair<int, int> pii;
 vector<pii> v[MAX_N];
 vector<int> dist(MAX_N, INF);
 
-//N: 정점 갯수, E 간선의 갯수
-int N, E, i;
+int N, E, i;	//N: 정점 갯수, E 간선의 갯수
 
 int dijkstra(int st) {
 
-	for(i=0; iM)
+	for (i = 0; i < N; i++) {
+		dist[i] = INF;
+	}
+	
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	pq.push({ st, 0 });
+	dist[st] = 0;
 
 
-	return 
+	while (!pq.empty()) {
+		int now = pq.top().first;
+		int nowCost = pq.top().second;
+
+		pq.pop();
+
+		for (i = 0; i < v[now].size(); i++) {
+
+			int next = v[now][i].first;
+			int nextCost = nowCost + v[now][i].second;
+
+			if (dist[next] > nextCost) {
+				dist[next] = nextCost;
+				pq.push({ next, nextCost });
+			}
+		}
+	}//while
+
+
+	return dist[N];
 }
 
 int main() {
 
 	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_1504.txt", "r", stdin);
 	freopen("D:/Git/SWCert/input/BOJ_1504.txt", "r", stdin);
-
-
-	scanf("%d %d", &N, &E);
+	scanf("%d %d", &N, &E);	//N: 정점 갯수, E 간선의 갯수
 
 	//a: 시작, b: 도착, c:길이
 	int a, b, c;
 	for (i = 1; i <= N; i++) {
 		scanf("%d %d %d", &a, &b, &c);
 		v[a].push_back({ b, c });
-		v[b].push_back({ a, c });
+		v[b].push_back({ a, c });		//양방향
 	}
 
-	//v1, v2 거처야하는 정점
-	int v1, v2, ans = 0;
-	scanf("%d %d", &v1, &v2);
+	int v1, v2;	//반드시 거쳐야하는 정점
 
-	printf("%d\n", dist[N]);
+	printf("%d\n", dijkstra(1));
 
 	return 0;
 }
