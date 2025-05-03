e38218af82b5842a85566019cc4100c75c689aa1
choiyunseok <choiyunseok@gmail.com>
2025-05-03 20:36:26 +0900

    250503_GPC

--- BOJ/12834.cpp
+++ BOJ/12834.cpp
@@ -1,5 +1,5 @@
-//백준(BOJ) 1854 - K번째 최단경로 찾기
-//https://www.acmicpc.net/problem/1854
+//백준(BOJ) 12834 - 주간 미팅
+//https://www.acmicpc.net/problem/12834
 //Algo: Dijkstra
 //Solve: 
 
@@ -11,21 +11,29 @@
 #include <queue>
 using namespace std;
 
-#define MAX_N	1001
+#define MAX_N	101
 #define INF		1e9
 
 typedef pair<int, int> pii;
 vector<pii> v[MAX_N];
 int dist[MAX_N], i;
-
+int N, V, E;	//기사단 팀원의 수 N, 장소의 수 V, 도로의 수 E
+int A, B
 
 void dijkstra(int st) {
 
-	
+
 }
 
 
 int main() {
+
+	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_12834.txt", "r", stdin);
+	freopen("D:/Git/SWCert/input/BOJ_12834.txt", "r", stdin);
+
+	int A, B;	//KIST의 위치 A와 씨알푸드의 위치 B
+	scanf("%d %d %d", &N, &V, &E);	//기사단 팀원의 수 N, 장소의 수 V, 도로의 수 E
+	scanf("%d %d", &A, &B);
 
 
 	return 0;
