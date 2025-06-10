3fffa2cc228a74318f84b24a1ae23d4586b54d56
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2025-06-10 20:51:59 +0900

    250610_Mac
    250610_Mac

---
+++ BOJ/18352_PQ.cpp
@@ -0,0 +1,33 @@
+//https://www.acmicpc.net/problem/18352
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+typedef pair<int, int> pii;
+
+#define MAX_N   300001
+#define 1e9     1e9 
+
+int main() {
+
+
+	freopen("/Users/timchoi/Git/SWCert/input/BOJ_18352.txt", "r", stdin);
+
+    //도시의 개수 N, 도로의 개수 M, 거리 정보 K, 출발 도시의 번호 X
+    int N, M, K, X;
+    scanf("%d %d %d %d", &N, &M, &K, &X);
+
+    int A, B;
+
+    vector<int> 
+    for(int i=0; i<M; i++) {
+        scanf("%d %d", &A, &B);
+
+    }
+
+    return 0;
+}
