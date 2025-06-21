0dc4beaa5470f4d2aeacbc2a357fdddf22b0163f
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2025-06-21 12:14:07 +0900

    250621_Mac
    250621_Mac

--- BOJ/18352_PQ.cpp
+++ BOJ/18352_PQ.cpp
@@ -23,11 +23,20 @@ int main() {
 
     int A, B;
 
-    vector<int> 
+    vector<int> v[MAX_N];
+    int dist[MAX_N];
     for(int i=0; i<M; i++) {
         scanf("%d %d", &A, &B);
-
+        v[A].push_back(B);
     }
+
+    priority_queue<pii> pq;
+    pq.push({X, 0})
+    for(int i=0; i<N; i++) {
+        
+    }
+
+
 
     return 0;
 }
