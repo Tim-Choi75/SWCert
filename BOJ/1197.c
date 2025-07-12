0cd980fa2770c0a076ffd3f9e345a7c29954396f
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2025-07-12 14:26:26 +0900

    250712_Mac
    250712_Mac

--- BOJ/1197.c
+++ BOJ/1197.c
@@ -1,3 +1,4 @@
+//https://www.acmicpc.net/problem/1197
 //https://minwook6457.tistory.com/6
 
 #include <stdio.h>
@@ -8,11 +9,32 @@
 
 typedef struct edge {
     int start, end, weight;
+} edge;
+
+int compare(const void* a, const void* b) {
+
+    struct edge* x = (struct edge*)a;
+    struct edge* y = (struct edge*)b;
+    return x->weight-y->weight;
 }
+
 
 int parent[MAX_N];
 edge E[MAX_N];
 
+int find(int x) {
+    if(x == parent[x]) {
+        return x;
+    } else {
+        return parent[x] = find(parent[x]);
+    }
+}
+
+void union_find(int a, int b) {
+    a = parent[a];
+    b = parent[b];
+    parent[a] = b;
+}
 
 int main() {
 
@@ -32,14 +54,27 @@ int main() {
         parent[i]=i;
    }
 
-   //가중치 기준으로 정렬ㄴ 
+   //가중치 기준으로 정렬
    qsort(E, e, sizeof(struct edge), compare);
 
    int j = 0;
    int accepted = 0;
 
+   int a, b, result = 0;
 
+   while(accepted != v-1) {//크루스칼 알고리즘 적용
+     a = E[j].start;
+     b = E[j].end;
 
+    if(find(a) != find(b)) {
+        union_find(a, b);
+        accepted++;
+        result += E[j].weight;
+    }
+    j++;
 
+   }//while
+   printf("%d", result);
+   return 0;
 
 }
