aed4760b8a5f8aabe4ec0e9f31fbe11c1f8b50ab
choiyunseok <choiyunseok@gmail.com>
2025-07-14 22:40:55 +0900

    250714_GPC
    250714_GPC

---
+++ Algo/Krusckal.cpp
@@ -0,0 +1,27 @@
+//https://swblossom.tistory.com/51
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+class Edge {
+public:
+    int node[2];
+    int distance;
+    Edge(int a, int b, int distance) {
+        this->node[0] = a;
+        this->node[1] = b;
+    }
+
+
+
+
+
+
+}
+
+
+int main() {
+    vector <Edge> v;
+    v.push_back(Edge(1, 7, 12));
+}
