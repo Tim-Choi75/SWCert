d395476e0c56ab05b84d98d581c1735c603ac6b7
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2025-07-27 18:42:00 +0900

    250727_Mac

---
+++ DataStructure/UnorderdMap_Test2.cpp
@@ -0,0 +1,23 @@
+//https://tinyurl.com/2dd7tb3r
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    unordered_map<string, int> umap;    //create unordered_map of string, pair
+
+    //add key pair value
+    umap{"apple"} = 5;
+    umap{"banana"} =3;
+    
+
+
+
+
+
+
+
+}
