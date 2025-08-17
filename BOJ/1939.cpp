98a4d2b7aa86178027634bd12a016e538f65684d
choiyunseok <choiyunseok@gmail.com>
2025-08-17 20:45:49 +0900

    250817_GPC
    250817_GPC

--- BOJ/1939.cpp
+++ BOJ/1939.cpp
@@ -1,7 +1,7 @@
 //백준 1939 - 중량제한
 //https://www.acmicpc.net/problem/1939
 //Algo: Dijkstra
-//Solve: https://hagisilecoding.tistory.com/139
+//Solve: https://stritegdc.tistory.com/198
 
 
 #include <iostream>
@@ -10,9 +10,25 @@
 #include <cstring>
 using namespace std;
 
-// N, M(1 ≤ M ≤ 100,000)이 주어진다. 다음 M
+#define pii pair<int, int>
 
-int main(){
+//N(2 ≤ N ≤ 10,000), M(1 ≤ M ≤ 100,000)이 주어진다. 다음 M
+int N, M;
 
-    return 0;
+vector<pii> node[10001];
+
+int main() {
+
+	scanf("%d %d", &N, &M);
+
+	int A, B, C; //A에서 B로 가는 중량 C
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d %d", &A, &B, &C);
+		node[A].push_back({ B, C });
+		node[B].push_back({ A, C });
+	}
+
+	int U, V;	//U->V 공장으로 이동
+
+	return 0;
 }
