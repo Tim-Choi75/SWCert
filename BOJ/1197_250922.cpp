e28da89b5e7abccaf3f83ca2bcd2951ca83b1e4c
choiyunseok <choiyunseok@gmail.com>
2025-09-23 00:16:48 +0900

    250923_GPC
    250923_GPC

---
+++ BOJ/1197_250922.cpp
@@ -0,0 +1,22 @@
+//백준(BOJ) 1197 - 최소 스패닝 트리
+//https://www.acmicpc.net/problem/1197
+//Algo: MST
+//Solve: 
+
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+	//freopen("/Users/timchoi/Git/SWCert/input/BOJ_1197.txt", "r", stdin);
+	freopen("D:/Git/SWCert/input/BOJ_1197.txt", "r", stdin);
+
+	int vertex, e;
+	printf("%d %d", &vertex, &e);
+	int result
+
+	return 0;
+}
