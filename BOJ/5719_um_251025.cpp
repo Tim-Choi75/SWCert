7e4f2465ff5d8630b273b11cde275e7a63ff9d04
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2025-10-25 12:32:08 +0900

    251025_Mac
    251025_Mac

---
+++ BOJ/5719_um_251025.cpp
@@ -0,0 +1,57 @@
+//백준(BOJ) 5719 - 거의 최단 경로
+//https://www.acmicpc.net/problem/5719
+//Algo: Dijkstra
+//Solve: https://unfunhy.tistory.com/23
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+//장소의 수 N (2 ≤ N ≤ 500), 도로의 수 M (1 ≤ M ≤ 10000)
+
+#define MAX_N 500
+#define MAX_M 1000
+
+int N, N, S, D;	
+
+
+void input() {
+
+
+	
+	
+
+	int U, V, P;	//U에서 V로 가는 도로의 길이가 P라
+	for (int i = 0; i < MAX_N; i++) {
+
+
+	}
+
+
+}
+
+int main() {
+
+	freopen("/Users/timchoi/Git/SWCert/input/BOJ_5719.txt", "r", stdin);
+	//freopen("D:/Git/SWCert/input/BOJ_5719.txt", "r", stdin);
+
+    //장소의 수 N (2 ≤ N ≤ 500), 도로의 수 M (1 ≤ M ≤ 10000)
+    while(true) {
+        scanf("%d %d", &N, &M);
+        if(N+M ==0)
+        break;
+
+        scanf("%d %d", &S, &D);	//S: 시작, D: 도착
+
+        for (int i = 0; i < N; i++) {
+
+
+    }
+	return 0;
+}
