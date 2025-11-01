b881b81065581048aeb4b9439150513a09a0810e
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2025-11-01 15:27:03 +0900

    251101_Mac
    251101_Mac

--- BOJ/13911.cpp
+++ BOJ/13911.cpp
@@ -1,7 +1,7 @@
 //백준(BOJ) 13911 - 집구하기
 //https://www.acmicpc.net/problem/13911
 //Algo: Dijkstra
-//Solve:
+//Solve: https://yabmoons.tistory.com/360
 
 
 #define  _CRT_SECURE_NO_WARNINGS
@@ -13,6 +13,12 @@
 
 using namespace std;
 
+#define MAX_N 100010
+#define M_Parent 10001
+#define M_Parent 10001
+#define INF		 1e9
+
+int V, E, M_Num, S_Num
 
 
 int main() {
