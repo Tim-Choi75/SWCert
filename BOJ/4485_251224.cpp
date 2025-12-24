51234ac7b9fe3f27402a9170e81727f1340c371e
‘choiyunseok@gmail.com’ <bluet75@naver.com>
2025-12-24 20:17:15 +0900

    251224_Mac
    251224_Mac

---
+++ BOJ/4485_251224.cpp
@@ -0,0 +1,29 @@
+//πÈ¡ÿ 4485-≥Ïªˆ ø  ¿‘¿∫ æ÷∞° ¡©¥Ÿ¡ˆ?
+//https://www.acmicpc.net/problem/4485
+//Algo: Dijkstra
+//Solu: https://yabmoons.tistory.com/388
+//		https://kau-algorithm.tistory.com/1206
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_N 126
+#define INF 1e9
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int
+
+void dijkstra() {
+
+	
+}
+
+int main()
+{
+	freopen("/Users/timchoi/Git/SWCert/input/BOJ_4485.txt", "r", stdin);
+	//freopen("D:/Git/SWCert/input/BOJ_4485.txt", "r", stdin);
+
+	return 0;
+}
